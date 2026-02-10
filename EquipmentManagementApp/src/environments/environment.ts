2838ad74adc8403b19ecd2378c290f172e10c3ec
Leonid Nisanov <leonid72@gmail.com>
2026-02-10 07:13:20 +0200

    Add docker file

--- EquipmentManagementApp/src/environments/environment.ts
+++ EquipmentManagementApp/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:50556/api' // Replace with your actual API URL
+  // production: false,
+  // apiUrl: 'https://localhost:50556/api' // Replace with your actual API URL
+  production: true,
+  apiUrl: '/api'
 };
